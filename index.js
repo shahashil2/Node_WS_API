7c85d8b3734a0276606e728f0db0f14dddbc04e9
dipak.singh <dipak.singh@SA-MAC-015.local>
2017-01-13 17:12:18 +0530

    Echo Functionality Done With Body Parser

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-let express = require('express')
+var express = require("express");
 
-let app = express()
+var bodyParser = require("body-parser");
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+
 
 app.listen(1991, function () {
-  console.log('Example app listening on port 1991!')
-})
+  console.log('Example app listening on port 1991!');
+});
 
 
 app.get('/demoGet', function (req, res) {
-  res.send('Get Request Hit')
-})
+  // res.send('Get Request Hit')
+  res.send(req.body);
+});
 
 
 app.post('/demoPost', function (req, res) {
-  res.send('Post Request Hit')
-})
+  res.send(req.body);
+});
 
 
 app.put('/demoPut', function (req, res) {
-  res.send('Put Request Hit')
-})
+  res.send('Put Request Hit');
+});
 
 app.delete('/demoDelete', function (req, res) {
-  res.send('Delete Request Hit!')
-})
+  res.send('Delete Request Hit!');
+});
 
