ca9a5a133bacb0db2cad67861cfcda9fe63c2337
ashil.shah <ashil.shah@SA-MAC.local>
2017-01-13 15:58:16 +0530

    First Commit Of WS API

---
+++ index.js
@@ -0,0 +1,27 @@
+let express = require('express')
+
+let app = express()
+
+app.listen(1991, function () {
+  console.log('Example app listening on port 1991!')
+})
+
+
+app.get('/demoGet', function (req, res) {
+  res.send('Get Request Hit')
+})
+
+
+app.post('/demoPost', function (req, res) {
+  res.send('Post Request Hit')
+})
+
+
+app.put('/demoPut', function (req, res) {
+  res.send('Put Request Hit')
+})
+
+app.delete('/demoDelete', function (req, res) {
+  res.send('Delete Request Hit!')
+})
+
