dc42211891ce21956876ba5fc5e396f0d97433f6
dipak.singh <dipak.singh@SA-MAC-015.local>
2017-01-18 14:45:44 +0530

    Authentication function removed from server.js file

--- server.js
+++ server.js
@@ -22,29 +22,6 @@ app.use(bodyParser.json());
 
 
 var apiRoutes = require('./app/routes/api'); //get api routes
-
-apiRoutes.use(function(req,res,next){
-
-    console.log('Authentication method called');
-    var token = req.body.secret_token || req.query.secret_token || req.headers['x-access-token'];
-    //decode token
-    if(token){
-        // verifies secret
-        jwt.verify(token,config.secret,function(err,decode){
-            if(err){
-                res.json({success:false,message:'Invalid Token'});
-            }else{
-                req.decode =  decode;
-                next();
-            }
-        });
-
-    }else{
-        res.status(403).json({success:false,message:'No token provided'}); //403:Forbidden and user is not authorize to view any data
-    }
-});
-
-
 app.use('/api',apiRoutes);
 
 var db = mongoose.connection;
