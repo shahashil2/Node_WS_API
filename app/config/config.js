66bdf9b071cb5ab1e14a1f9fa072be5f06e99021
dipak.singh <dipak.singh@SA-MAC-015.local>
2017-01-20 17:14:53 +0530

    After changes to nonworking copy

---
+++ app/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret' : 'nodejsworkssmoothly',
+    'database' : 'mongodb://192.168.1.69:27017/EMSSysDb/'
+};
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 //-----------------------------------------
     //get the packages
 //-----------------------------------------
-require('dotenv').config() // get our config file
+
 const express     = require('express');
 const app         = express();
 const bodyParser  = require('body-parser');
 const mongoose    = require('mongoose'); //interact with our MongoDB database
-const jwt         = require('jsonwebtoken'); // used to create, sign, and verify tokens 
+const jwt         = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const config      = require('./app/config/config'); // get our config file
 const User        = require('./app/models/user'); // get our mongoose model
 
 //-----------------------------------------
     //Configuration
 //-----------------------------------------
 var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
-
-mongoose.connect(process.env.DB_HOST); // connect to database
-app.set('userSecret',process.env.SECRET_TOKEN); // secret variable 
+mongoose.connect(config.database); // connect to database
+app.set('userSecret',config.secret); // secret variable 
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
