24b1717660879bc32b925c5e547a8b90b9ecb460
dipak.singh <dipak.singh@SA-MAC-015.local>
2017-01-20 17:31:33 +0530

    Environment Variable Added again after mishap in branches.

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 //-----------------------------------------
     //get the packages
 //-----------------------------------------
-
+require('dotenv').config() // get our config file
 const express     = require('express');
 const app         = express();
 const bodyParser  = require('body-parser');
 const mongoose    = require('mongoose'); //interact with our MongoDB database
 const jwt         = require('jsonwebtoken'); // used to create, sign, and verify tokens
-const config      = require('./app/config/config'); // get our config file
 const User        = require('./app/models/user'); // get our mongoose model
 
 //-----------------------------------------
     //Configuration
 //-----------------------------------------
 var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
-mongoose.connect(config.database); // connect to database
-app.set('userSecret',config.secret); // secret variable 
+mongoose.connect(process.env.DB_HOST); // connect to database
+app.set('userSecret',process.env.SECRET_TOKEN); // secret variable 
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
